ce1888ab16734ce1541f4677bce0e22f09d6985e
DiogoSemedo <43584520+DiogoSemedo@users.noreply.github.com>
2018-10-12 15:36:03 +0100

    Add files via upload

--- project/src/Database.java
+++ project/src/Database.java
@@ -2,7 +2,68 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Database {
+    public class User{
+        private String username;
+        private String password;
+        private boolean permissao; //false-normal true-editor
+
+        public User(String username, String password, boolean permissao) {
+            this.username = username;
+            this.password = password;
+            this.permissao = permissao;
+        }
+    }
+    private HashMap<String,String> commands = new HashMap<String,String>();
+    public void initCommands(){
+        commands.put("type","regist");
+        commands.put("type","login");
+    }
+    private ArrayList <user> users = new ArrayList<>();
+    public String process(String message){
+        String [] sm = message.split(";");
+        String [] type = sm[0].split("|");
+        String reply ="";
+        if(commands.containsValue(type[1])) {
+            switch (type[1]) {
+                case "regist":
+                    reply=regist(sm);
+                    break;
+                case "login":
+                    break;
+                default:
+                    break;
+            }
+        }
+        return reply;
+    }
+    public String regist(String [] message){
+        try{
+            if(message.size()==2){//exemplo --> type|regist;username|__name__;password|__pass__;
+                String [] username = message[1].split("|");
+                String [] password = message[2].split("|");
+            }
+            else{
+              return "Wrong format of request";
+            }
+        }catch(Exception e){
+            return "Wrong format of request";
+        }
+        //verificar a validade do request
+        if(username[0].equals("username") && password[0].equals("password")){
+            User u = new User(username[1],password[1],false);
+            //verificamos se o username nao existe
+            if(!users.contains(u)){
+                re
+            }
+        }
+        return "";
+    }
+}
+    /*
     public static void main(String[] args){
         try{
             Class.forName("org.postgresql.Driver");
@@ -15,5 +76,4 @@ public class Database {
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
-    }
-}
+    }*/
--- project/src/MulticastServer.java
+++ project/src/MulticastServer.java
@@ -6,35 +6,32 @@ import java.io.IOException;
 public class MulticastServer extends Thread {
     private String MULTICAST_ADDRESS = "224.0.224.0";
     private int PORT = 4321;
-    private long SLEEP_TIME = 10000;
 
     public static void main(String[] args) {
         MulticastServer server = new MulticastServer();
         server.start();
 
     }
-
     public MulticastServer() {
         super("Server " + (long) (Math.random() * 1000));
     }
 
     public void run() {
-        MulticastSocket socket = null;
+        MulticastSocket listen = null;
         MulticastSocket reply = null;
         long counter = 0;
         String messageReply = "teste";
         System.out.println(this.getName() + " running...");
         try {
-            socket = new MulticastSocket(PORT);  // create socket without binding it (only for receving)
+            listen = new MulticastSocket(PORT);  // create socket without binding it (only for receving)
             reply = new MulticastSocket();
             InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
-            socket.joinGroup(group);
+            listen.joinGroup(group);
             reply.joinGroup(group);
-
             while (true) {
                 byte[] buffer = new byte[256];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                socket.receive(packet);//espera até receber
+                listen.receive(packet);//espera até receber
                 if(reply.getLocalPort() != packet.getPort()) {
                     System.out.println("Recebi: " + packet.getAddress().getHostAddress() + ":" + packet.getPort());
                     String message = new String(packet.getData(), 0, packet.getLength());
@@ -47,13 +44,11 @@ public class MulticastServer extends Thread {
                     //testar funcao packet.setAddress
                     reply.send(packet2);
                 }
-
-
             }
         }catch (IOException e) {
             e.printStackTrace();
         } finally {
-            socket.close();
+            listen.close();
         }
     }
     /*
