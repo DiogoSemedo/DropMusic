2602b33971b6549f2ff9a5407bed9a86e35774bf
DiogoSemedo <43584520+DiogoSemedo@users.noreply.github.com>
2018-12-13 17:52:05 +0000

    Add files via upload

---
+++ meta2/src/action/LoginAction.java
@@ -0,0 +1,47 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+import model.User;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.struts2.interceptor.SessionAware;
+
+import java.util.Map;
+
+public class LoginAction extends ActionSupport implements SessionAware, ModelDriven {
+    private User user = new User();
+    private Map<String,Object> session;
+    @Override
+    public void validate(){
+        if(StringUtils.isEmpty(user.getUsername())){
+            addFieldError("username","User ID cannot be blank");
+        }
+        if(StringUtils.isEmpty(user.getPassword())){
+            addFieldError("password","Password cannot be blank");
+        }
+    }
+
+    public String execute(){
+        if(user.getUsername().equals("ola") && user.getPassword().equals("adeus"))
+            return SUCCESS;
+        return LOGIN;
+    }
+
+    @Override
+    public void setSession(Map<String, Object> map) {
+        this.session = map;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Object getModel() {
+        return user;
+    }
+}
---
+++ meta2/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: diogo
+  Date: 13/12/2018
+  Time: 14:53
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<html>
+  <head>
+    <title>Ola</title>
+  </head>
+  <body>
+  </body>
+</html>
